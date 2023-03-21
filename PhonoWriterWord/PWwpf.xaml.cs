09e6b1fcc6f9299e9c19f3b0c70093685a95c482
Nathan Dely <nathan.dely@gmail.com>
2023-03-21 10:17:13 +0100

    Get the word when the cursor in on it and listview

--- PhonoWriterWord/PWwpf.xaml.cs
+++ PhonoWriterWord/PWwpf.xaml.cs
@@ -24,5 +24,26 @@ namespace PhonoWriterWord
         {
             InitializeComponent();
         }
+
+        private void ListViewItem_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            var item = sender as ListViewItem;
+            if (item != null && item.IsSelected)
+            {
+                System.Diagnostics.Debug.WriteLine(item.ToString());
+            }
+        }
+        private void ListViewItem_EnterPressed(object sender, KeyEventArgs e)
+        {
+            var item = sender as ListViewItem;
+            if (item != null && item.IsSelected && e.Key == Key.Return)
+            {
+                ThisAddIn.KeyReturnPressed(item);
+            }
+            if (e.Key == Key.Space)
+            {
+                myList.Items.Add("uwu");
+            }
+        }
     }
 }
